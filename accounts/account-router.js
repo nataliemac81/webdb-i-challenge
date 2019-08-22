e358706479e06e2b150c1f96352555581081d1a8
Natalie McCroy <nataliemccroy@gmail.com>
2019-08-21 20:26:33 -0500

    POST, PUT, DELETE endpoints all working

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -39,5 +39,60 @@ router.get('/:id', (req, res) => {
     })
 });
 
+router.post('/', (req, res) => {
+    // INSERT INTO Accounts (all of the keys from req.body) VALUES (all of the values from req.body)
+    const acctData = req.body
+
+    db('accounts').insert(acctData)
+    .then(ids => {
+        res.status(201).json({ newAcctId: ids[0]})
+    })
+    .catch(err => {
+        res.status(500).json({
+            err: err,
+            message: 'There was an error creating the account'
+        })
+    })
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+
+    db('accounts').where({ id }).update(changes)
+    .then(count => {
+        if (count) {
+            res.json({ updated: count })
+        } else {
+            res.status(404).json({ message: 'invalid account id'})
+        }      
+    })
+    .catch(err => {
+        res.status(500).json({
+            err: err,
+            message: 'There was an error creating the account'
+        }) 
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+
+    db('accounts').where({ id }).del()
+    .then(count => {
+        if (count) {
+            res.json({ deleted: count })
+        } else {
+            res.status(404).json({ message: 'invalid account id'})
+        }      
+    })
+    .catch(err => {
+        res.status(500).json({
+            err: err,
+            message: 'There was an error deleting the account'
+        }) 
+    })
+});
+
 
 module.exports = router;
